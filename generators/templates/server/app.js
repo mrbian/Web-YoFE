a820bc732a227fd6ccd5cad0ed6f23a3b4da9986
卞留念 <554565808@qq.com>
2016-09-01 16:29:02 +0800

    修复只能本地查看的bug，添加exports等依赖

--- generators/templates/server/app.js
+++ generators/templates/server/app.js
@@ -94,12 +94,14 @@ app.use(serve(staticDir, {
 
 app = http.createServer(app.callback());
 
-app.listen(pkg.localServer.port, "127.0.0.1", () => {
-    var url = util.format("http://%s:%d", "localhost", pkg.localServer.port);
+app.listen(pkg.localServer.port);
 
-    console.log("Listening at %s", url);
-
-    // open(url);
-});
+// app.listen(pkg.localServer.port, "127.0.0.1", () => {
+//     var url = util.format("http://%s:%d", "localhost", pkg.localServer.port);
+//
+//     console.log("Listening at %s", url);
+//
+//     // open(url);
+// });
 
 module.exports = app;
